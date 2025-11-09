f52289767642fce435b1f1ed6b33db152c39810f
PAKAPON CHAIMONGKOLTRAKUL <jedai2543@gmail.com>
2025-11-09 21:39:08 +0700

    add cors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from api import user, product, category, review
 
 app = FastAPI()
@@ -7,3 +9,24 @@ app.include_router(user.router, prefix="/api/v1")
 app.include_router(product.router, prefix="/api/v1")
 app.include_router(category.router, prefix="/api/v1")
 app.include_router(review.router, prefix="/api/v1")
+
+origins = [
+    "http://localhost:5173",
+    "localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.get("/", tags=["Root"])
+def read_root():
+    """
+    A simple root endpint to confirm that api is running
+    """
+
+    return { "message" : "Welcome to E-commerve API"}
