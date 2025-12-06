9f12fbc5c4ca042cdc203173cb6ab3c48dfa8073
PAKAPON CHAIMONGKOLTRAKUL <jedai2543@gmail.com>
2025-12-06 08:11:07 +0700

    add get current user 37.11

--- core/auth.py
+++ core/auth.py
@@ -8,6 +8,7 @@ from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from decouple import config #need to install python-decouple
+from core.db import get_session
 
 SECRET_KEY = config("SECRET_KEY")
 ALGORITHM = config("ALGORITHM")
@@ -25,4 +26,21 @@ def create_accessL_token(data: dict,expires_delta: Optional[timedelta] = None):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
+def get_current_user( #to create a user need to verify 1.token 2.session
+    token: Annotated[str, Depends(oauth2_schema)],
+    session: Annotated[Session, Depends(get_session)]
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credential",
+        headers={"WWW-Authenticate":"Bearer"}
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
 
+#37.11
